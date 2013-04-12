8996202e8058ef1017fa561b9affcc73b63ca829
Matt Watkajtys <watkajtys@gmail.com>
2013-04-12 18:34:44 -0400

    Initial commit. Sorcery Installed. Set to go. '

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelpQ::Application.config.secret_token = '401361bf2f410e761c8bb85410821a0487af3fda7f33c8106d0f2a8b78031298eea1a294c93ea85588dbf6aaf2874c21d494f765ddbc3f00c0af6b9c72a89f6d'
